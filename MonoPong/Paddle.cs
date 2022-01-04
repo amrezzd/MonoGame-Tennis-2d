abd3c92fc064eea3f102c01a78532517db58d3d1
Amir Rezazadeh <amrezzd@gmail.com>
2022-01-04 09:24:48 +0330

    Move Sprite2D to separate file

--- MonoPong/Paddle.cs
+++ MonoPong/Paddle.cs
@@ -37,38 +37,4 @@ namespace MonoPong
             position.Y = yLocation;
         }
     }
-
-    internal abstract class Sprite2D
-    {
-        private readonly Texture2D texture;
-
-        protected Vector2 velocity = Vector2.Zero;
-        protected Vector2 position;
-        public Vector2 Position { get { return position; } }
-
-        public int Height { get { return texture.Height; } }
-
-        public float Width { get { return texture.Width; } }
-
-        public Sprite2D(Texture2D load, Vector2 location)
-        {
-            this.texture = load;
-            this.position = location;
-        }
-
-        internal void Draw(SpriteBatch spriteBatch)
-        {
-            spriteBatch.Draw(texture, position, Color.White);
-        }
-
-        internal virtual void Update(GameTime gameTime)
-        {
-            position += velocity;
-            CheckBounds();
-        }
-
-        protected abstract void CheckBounds();
-
-
-    }
 }
---
+++ MonoPong/Sprite2D.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoPong
+{
+    internal abstract class Sprite2D
+    {
+        private readonly Texture2D texture;
+
+        protected Vector2 velocity = Vector2.Zero;
+        protected Vector2 position;
+        public Vector2 Position { get { return position; } }
+
+        public int Height { get { return texture.Height; } }
+
+        public float Width { get { return texture.Width; } }
+
+        public Sprite2D(Texture2D load, Vector2 location)
+        {
+            this.texture = load;
+            this.position = location;
+        }
+
+        internal void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+        }
+
+        internal virtual void Update(GameTime gameTime)
+        {
+            position += velocity;
+            CheckBounds();
+        }
+
+        protected abstract void CheckBounds();
+
+
+    }
+}
